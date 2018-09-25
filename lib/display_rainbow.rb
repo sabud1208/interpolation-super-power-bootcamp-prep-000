8a910ec616bec6ab31d4ec2ea3dd933d5a66bb60
STEPHANIE MESSAD kOUCHICH <sabud1208@gmail.com>
2018-09-25 03:58:15 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -5,5 +5,3 @@ def display_rainbow(colors)
   puts 'R: #{colors[0], O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}'
 end
 rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
-
-display_rainbow(colors)
